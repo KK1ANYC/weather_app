bc22629333b79a0271520582b58fc08623926c84
KK1ANYC <kevin.kim9411@gmail.com>
2021-10-30 17:17:37 -0400

    install axios

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
       </header>
     </div>
   );
