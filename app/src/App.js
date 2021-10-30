8515203ccf9a8fa6728ab3d507d7b24e1d5cc437
KK1ANYC <kevin.kim9411@gmail.com>
2021-10-30 17:19:35 -0400

    frontend app/src/App created a useEffect to make api calls

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,17 @@ import axios from "axios";
 const App = () => {
   const [weather, setWeather] = useState([]);
 
-  
+  useEffect(() => {
+    const getWeather = async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=40.7516&lon=-73.9755&appid=323b57d6a96e11f1f8c51778e23170c7`
+      );
+      setWeather(data.list);
+    };
+    getWeather();
+  }, []);
+
+  console.log("weather", weather);
 
   return (
     <div className="App">
