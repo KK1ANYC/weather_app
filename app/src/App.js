9ec812c9623a2e69c7bc4c17acc753ac0b0b44bc
KK1ANYC <kevin.kim9411@gmail.com>
2021-10-30 17:18:22 -0400

    set up useState weather

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function App() {
+const App = () => {
+  const [weather, setWeather] = useState([]);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <header className="App-header"></header>
+      <div></div>
     </div>
   );
-}
+};
 
 export default App;
